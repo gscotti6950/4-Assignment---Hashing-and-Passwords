f90b450d57419f6264b877ebd8fbcf58a07f208d
ab-arias <abarias@calpoly.edu>
2022-10-17 19:41:51 -0700

    get unique words from hobbit.txt

--- breakingHashes.py
+++ breakingHashes.py
@@ -1,36 +1,26 @@
-# import requests
-from bcrypt import hashpw
-
-# def uniqueWords(text):
-#     unique_words = []
-#     for word in text:
-#         if word not in unique_words:
-#             unique_words.append(word)
-#     for x in unique_words:
-#         print(x)
-
-# hobbit_dir = "https://archive.org/stream/dli.ernet.474126/474126-The%20Hobbit%281937%29_djvu.txt"
-# response = requests.get(hobbit_dir, "b")
-# hobbit_text = response.text
-
-# print(hobbit_text)
+from bcrypt import hashpw, checkpw
+import string
 
 
+def uniqueWords():
+    file = "./hobbit.txt"
+    text = open(file, "r").readlines()
+    split_text = []
+    words = []
 
-# password = b"super secret password"
-# # Hash a password for the first time, with a randomly-generated salt
-# hashed = bcrypt.hashpw(password, )
-# # Check that an unhashed password matches one that has previously been
-# # hashed
-# if bcrypt.checkpw(password, hashed):
-#     print("It Matches!")
-# else:
-#     print("It Does not Match :(")
+    for line in text:
+        split_text.append(line.split(" "))
 
-# def myHash(text, salt):
-#     print()
+    for set in split_text:
+        for word in set:
+            words.append(word.translate(str.maketrans('', '', string.punctuation)).strip())
 
+    unique_words = []
+    for word in words:
+        if word not in unique_words:
+            unique_words.append(word)
 
+    print(len(unique_words))
 
 def parseShadow():
     file = "./shadow.txt"
@@ -55,18 +45,25 @@ def parseShadow():
     return store
     
 def password_cracker(shadow_data):
-     current = "$" + shadow_data[0]["algorithm"] + "$" + shadow_data[0]["work_factor"] + "$" + shadow_data[0]["salt"]
-     print(hashpw(b"registrationsucks", current.encode("utf-8")))
-     print(hashpw(b"registrationsucks", b"$2b$08$J9FW66ZdPI2nrIMcOxFYI."))
+    current = "$" + shadow_data[0]["algorithm"] + "$" + shadow_data[0]["work_factor"] + "$" + shadow_data[0]["salt"]
+    hashed = hashpw(b"The", current.encode("utf-8"))
+    password = b"The"
 
+    if checkpw(password, hashed) == True:
+        print("Password matches! The password is: " + password.decode("utf-8"))
+    else:
+        print("The passwords didn't match :(")
+    
+    #  print(hashpw(b"registrationsucks", current.encode("utf-8")))
+    #  print(hashpw(b"registrationsucks", b"$2b$08$J9FW66ZdPI2nrIMcOxFYI."))
+     
 
 
 def main():
-    shadow_data = parseShadow()
-    print(shadow_data[0])
-    password_cracker(shadow_data)
-
-
+    # shadow_data = parseShadow()
+    # print(shadow_data[0])
+    # password_cracker(shadow_data)
+    uniqueWords()
 
 if __name__ == "__main__":
     main()
