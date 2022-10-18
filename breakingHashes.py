94303ec105dbe3a7306ae72f02c75f136fd3a8e2
Griffin Scotti <griffinscotti@gmail.com>
2022-10-18 11:28:34 -0700

    added pass crack for the nltk words

--- breakingHashes.py
+++ breakingHashes.py
@@ -1,28 +1,34 @@
+#from curses.ascii import isalnum
 from bcrypt import hashpw, checkpw
+from nltk.corpus import words
 import string
+import re
 
 #This function finds all of the unique values in hobbit.txt
 def uniqueWords():
     file = "./hobbit.txt"
-    text = open(file, "r").readlines()
-    split_text = []
+    split_text = open(file, "rb").read().decode('utf_8').split()
+    #split_text = []
     words = []
 
     #loop through text to get words into list
-    for line in text:
-        split_text.append(line.split(" "))
+    # for line in text:
+    #     split_text.append(line.split(" "))
 
     #now loop through the list of lists to get only one list
     for set in split_text:
         for word in set:
             #get rid of any alphanumeric values
-            words.append(word.translate(str.maketrans('', '', string.punctuation)).strip())
+            #words = [t for t in split_text if re.match(r'[^\W\d]*$', t)]
+            if set.isalpha():
+                words.append(set)
     
     #now loop through the list in order to find all of the unique words
     unique_words = []
     for word in words:
-        if word not in unique_words and len(word) > 5 and len(word) < 11:
-            unique_words.append(word)
+        if word.lower() not in unique_words and len(word) > 5 and len(word) < 11:
+            unique_words.append(word.lower())
+    print(len(unique_words))
     return unique_words
 
 #This function parses through shadow.txt
@@ -49,39 +55,82 @@ def parseShadow():
             "salt": salt_split[i],
             "hash_value": hash_value[i]
         }
+    #print(store[1])
     return store
     
 #This function hashes all of the unique words found by uniqueWords() an dcompares it to the hash value found in shadow.txt
-def password_cracker(shadow_data, words):
+def password_cracker_hobbit(shadow_data, words):
+    i = 4
+    found = False
 
-    current = "$" + shadow_data[0]["algorithm"] + "$" + shadow_data[0]["work_factor"] + "$" + shadow_data[0]["salt"]
-    password = shadow_data[0]["hash_value"]
+    current = "$" + shadow_data[i]["algorithm"] + "$" + shadow_data[i]["work_factor"] + "$" + shadow_data[i]["salt"]
+    password = shadow_data[i]["hash_value"]
+    print(current+password)
 
     #iterate through all of the words
+    #passw = hashpw(b"hobbit", current.encode("utf-8"))
     for word in words:
         #hash the current word
-        hashed = hashpw(word.encode("utf-8"), current.encode("utf-8"))[29:]
+        #hashed = (hashpw(word.encode("utf-8"), current.encode("utf-8")))
 
         # if checkpw(password, hashed) == True:
         #     print(shadow_data[0]["name"] + " " + word)
 
         #checks if the hash of the current word matches the hash value from shadow.txt
-        if hashed == password:
+        if checkpw(word.encode("utf-8"), (current+password).encode("utf-8")):#hashed[29:] == password:
             #hooray print password for the user
-            print(shadow_data[0]["name"] + " " + word)
+            print(shadow_data[i]["name"] + " " + word)
+            found = True
+            break
         # else:
         #     print("Word tried: " + word)
         #     print("Hashed Value: " + password)
         #     print("Current Hash Value: " + hashed.decode("utf-8"))
 
     #if the password was not found print sad statement
-    print("password not found :(")
+    if not found:
+        password_cracker_full(shadow_data, i)
+        
+def password_cracker_full(shadow_data, thingy):
+    found = False
+    i = thingy
+
+    current = "$" + shadow_data[i]["algorithm"] + "$" + shadow_data[i]["work_factor"] + "$" + shadow_data[i]["salt"]
+    password = shadow_data[i]["hash_value"]
+    print(current+password)
+
+    #iterate through all of the words
+    #passw = hashpw(b"hobbit", current.encode("utf-8"))
+    for word in words.words():
+        #hash the current word
+        #hashed = (hashpw(word.encode("utf-8"), current.encode("utf-8")))
+
+        # if checkpw(password, hashed) == True:
+        #     print(shadow_data[0]["name"] + " " + word)
+
+        #checks if the hash of the current word matches the hash value from shadow.txt
+        if len(word) < 11 or len(word) > 5:
+            if checkpw(word.encode("utf-8"), (current+password).encode("utf-8")):#hashed[29:] == password:
+            #hooray print password for the user
+                print(shadow_data[i]["name"] + " " + word)
+                found = True
+                break
+        # else:
+        #     print("Word tried: " + word)
+        #     print("Hashed Value: " + password)
+        #     print("Current Hash Value: " + hashed.decode("utf-8"))
+
+    #if the password was not found print sad statement
+    if not found:
+        print("No password found >:(")
 
 
 def main():
     shadow_data = parseShadow()
-    words = uniqueWords()
-    password_cracker(shadow_data, words)
+    #words = uniqueWords()
+    #password_cracker_hobbit(shadow_data, words)
+    password_cracker_full(shadow_data, 4)
+    # test()
     
 
 if __name__ == "__main__":
